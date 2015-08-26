d3e35dd0c49127c9f5918fc54211591f25734529
Mecam <mecam6459@gmail.com>
2015-08-26 19:57:40 -0400

    jkmhnbgfvcdx

--- glfw/glfw/glfw.cpp
+++ glfw/glfw/glfw.cpp
@@ -9,6 +9,7 @@ void main()
 	int width;
 	int height;
 	unsigned char* image = SOIL_load_image("sample2.png", &width, &height, 0, SOIL_LOAD_RGB);
-	SOIL_free_image_data(image);
+	SOIL_free_image_data(image);  
+	int y;
 }
 
