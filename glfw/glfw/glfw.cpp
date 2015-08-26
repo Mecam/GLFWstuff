37b26cde789de0a92237ce67d6cdc46faa8c57af
Mecam <mecam6459@gmail.com>
2015-08-26 15:09:49 -0400

    herro

--- glfw/glfw/glfw.cpp
+++ glfw/glfw/glfw.cpp
@@ -4,9 +4,9 @@
 #include "stdafx.h"
 #include "Libraries\GLlibs\SOIL.h"
 
-
-void main()
-{
-    
+int width;
+int height;
+void main(){
+ unsigned char* image = SOIL_load_image("sample2.png", &width, &height, 0, SOIL_LOAD_RGB);
 }
 
