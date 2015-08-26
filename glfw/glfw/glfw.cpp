9f4b92d844c15833b35951c116f91c29cc22d0cc
Conner Tenn <connertenn@gmail.com>
2015-08-26 15:20:00 -0400

    UP

--- glfw/glfw/glfw.cpp
+++ glfw/glfw/glfw.cpp
@@ -4,9 +4,10 @@
 #include "stdafx.h"
 #include "Libraries\GLlibs\SOIL.h"
 
-int width;
-int height;
-void main(){
- unsigned char* image = SOIL_load_image("sample2.png", &width, &height, 0, SOIL_LOAD_RGB);
+void main()
+{
+	int width;
+	int height;
+	unsigned char* image = SOIL_load_image("sample2.png", &width, &height, 0, SOIL_LOAD_RGB);
 }
 
